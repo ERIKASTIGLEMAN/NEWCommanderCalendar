a9a9774b4eac483bab4a70a2281a65b33bf0db35
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-03-13 00:35:45 -0500

    Migration and Sample Data all set

--- Models/fakedata.sql
+++ Models/fakedata.sql
@@ -1,5 +1,9 @@
-TRUNCATE TABLE "Event";
+TRUNCATE TABLE "Events";
 
-INSERT INTO "Event" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('Dogs to Groomers', 'March 28,2021', '10:00am', 'Yellow', 'png', 'url', 'Do not forget leashes')
-INSERT INTO "Event" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('VolleyBall Practice', 'March 18,2021', '6:30pm', 'Green', 'png', 'url', 'Volunteer Hours Due')
-INSERT INTO "Event" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('Tennis Practice', 'March 21,2021', '5:00pm', 'Green', 'png', 'url', 'Set up additional lessons')
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('Dogs to Groomers', 'March 28,2021', '10:00am','Daily', 'Yellow', 'png', 'url', 'Do not forget leashes');
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('VolleyBall Practice', 'March 18,2021', '6:30pm', 'Daily', 'Green', 'png', 'url', 'Volunteer Hours Due');
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('Tennis Practice', 'March 21,2021', '5:00pm','Daily', 'Green', 'png', 'url', 'Set up additional lessons');
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('No School', 'March 27,2021', 'ALL DAY','Daily', 'Green', 'png', 'url', 'Teacher Conferences');
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('St Paddys Day', 'March 17,2021', 'time null','Holiday', 'Red', 'png', 'url', 'null');
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('Lightening vs Red Wings', 'March 30,2021', '7:30pm','Sport', 'Blue', 'png', 'url', 'grab jersey for gift');
+INSERT INTO "Events" ("EventName", "DateOfEvent", "TimeOfEvent", "TypeOfEvent", "ColorTag", "Image", "URL", "Notes") VALUES ('Weather', 'each day', 'time null','Weather', 'null', 'png', 'url', 'null');
