8178ad53962875a3b146ecc37c882521b7589424
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-03-16 07:34:16 -0400

    add new event button to calendar page

--- ClientApp/src/pages/MonthlyCalendarPages.jsx
+++ ClientApp/src/pages/MonthlyCalendarPages.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { MyCalendar } from '../components/MyCalendar'
 
 export function MonthlyCalendarPages() {
   return (
     <>
       <header className="monthly-giggles">
+        <button>
+          <Link to="/addnewevent">Add Event</Link>
+        </button>
         <blockquote>
           After I drink coffee I like to show the empty mug to the IT guy to
           tell him that I've successfully installed Java. He hates me...
